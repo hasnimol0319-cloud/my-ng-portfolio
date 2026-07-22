d4ed51bb56feddea017a79d65dbabc8bd1395ece
hasnimol0319-cloud <hasnimol0319@gmail.com>
2026-07-22 07:36:28 +0700

    Add project with navbar and footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('../components/home/home.component').then(m => m.HomeComponent)
+    },
+    {
+        path: 'about',
+        loadComponent: () => import('../components/about/about.component').then(m => m.AboutComponent)
+    },
+    {
+        path: 'not-found',
+        loadComponent: () => import('../components/not-found/not-found.component').then(m => m.NotFoundComponent)
+    },
+    {
+        path: '**',
+        redirectTo: 'not-found'
+    }
+];
