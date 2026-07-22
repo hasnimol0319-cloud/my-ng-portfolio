21dd27fab3e9a9cab08b8a17fc080adc0273e190
hasnimol0319-cloud <hasnimol0319@gmail.com>
2026-07-22 13:28:30 +0700

    Move components and layout folder to app folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,22 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from '../components/home/home.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
     {
-        path: '',
-        loadComponent: () => import('../components/home/home.component').then(m => m.HomeComponent)
+        path: 'home',
+        loadComponent: () => import('../app/components/home/home.component').then(m => m.HomeComponent)
     },
     {
         path: 'about',
-        loadComponent: () => import('../components/about/about.component').then(m => m.AboutComponent)
+        loadComponent: () => import('../app/components/about/about.component').then(m => m.AboutComponent)
     },
     {
         path: 'skill',
-        loadComponent: () => import('../components/skill/skill.component').then(m => m.SkillComponent)
+        loadComponent: () => import('../app/components/skill/skill.component').then(m => m.SkillComponent)
     },
     {
         path: 'not-found',
-        loadComponent: () => import('../components/not-found/not-found.component').then(m => m.NotFoundComponent)
+        loadComponent: () => import('../app/components/not-found/not-found.component').then(m => m.NotFoundComponent)
     },
     {
         path: '**',
