afe5d691d3d1c03019e8371aff5395851cf0d397
hasnimol0319-cloud <hasnimol0319@gmail.com>
2026-07-23 14:34:48 +0700

    Add data to solution section

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { map } from 'rxjs';
+
+interface Solution {
+  id: number,
+  title: string,
+  description: string
+}
 
 @Component({
   selector: 'app-home',
@@ -8,5 +16,18 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  solutions: Solution[] = [];
 
+  constructor(
+    private http: HttpClient
+  ) {}
+
+ngOnInit() {
+  this.getSolution();
+}
+  getSolution() {
+    this.http.get('/db/solutions.json').pipe(map((d:any) => d.solutions)).subscribe((res: Solution[]) => {
+      this.solutions = res;
+    })
+  }
 }
