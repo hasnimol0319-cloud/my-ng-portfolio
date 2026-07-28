1a93313855bb7dcd188da619a90c548eabd42f07
hasnimol0319-cloud <hasnimol0319@gmail.com>
2026-07-28 22:22:30 +0700

    Update contact page

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -24,6 +24,10 @@ export class ContactComponent {
   form: FormGroup;
   submitted = false;
   sending = false;
+  email = 'hasnimol0319@gmail.com';
+  phone = '+855 66 817 892';
+  location = 'Phnom Penh, Cambodia'
+  locationLink = 'https://www.google.com/maps/place/Toi/@11.5981772,104.8604297,49m/data=!3m1!1e3!4m6!3m5!1s0x310953b5147b7a47:0x26c42c22123aa276!8m2!3d11.5982825!4d104.8604314!16s%2Fg%2F11p13w_1gl?authuser=0&entry=ttu&g_ep=EgoyMDI2MDcyMi4wIKXMDSoASAFQAw%3D%3D';
 
   constructor(private fb: FormBuilder, private alertService: AlertService) {
     this.form = this.fb.group({
@@ -42,7 +46,6 @@ export class ContactComponent {
     this.submitted = true;
 
     if (this.form.invalid) {
-      this.alertService.info('Please fill the form to submit the message.')
       return;
     }
 
