a9dd31f2e5e378b2c490249fe42369b54a8ccff4
hasnimol0319-cloud <hasnimol0319@gmail.com>
2026-07-28 09:30:14 +0700

    Update about page

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { PromiseTabsComponent } from './promise-tabs/promise-tabs.component';
 import { SkillsComponent } from "./skills/skills.component";
+import { ExperiencesComponent } from "./experiences/experiences.component";
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [PromiseTabsComponent, SkillsComponent],
+  imports: [PromiseTabsComponent, SkillsComponent, ExperiencesComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
